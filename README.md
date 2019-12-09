b4a8481c1a40d1d7192ff0575a2833ef50b61797
juarezhenriquelisboa <juarezhenriquelisboa@gmail.com>
2019-12-08 20:29:20 -0500

    Atualização de softwares utilizados

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Script que rodo após a instalação do linux para instalar todos os programas que utilizo.
+
+--
+
+Script what I execute after the Linux installation with contains the softwares that I use.
--- script.sh
+++ script.sh
@@ -1,60 +1,25 @@
 #!/bin/bash
-#Version 0 
+#Version 1 
 
 	apt-get -y update
 
 	apt-get -y upgrade
 
-echo "Instalando pilha Mysql/Apache/PHP/PHPmyadmin:"
+	apt-get install software-properties-common
 
-	apt-get install mysql-server apache2 libapache2-mod-php php php-mysql phpmyadmin
+	apt-get install wget
 
-	echo -e '<?php phpinfo(); ?>' > /var/www/html/info.php
+	apt-get install curl
 
-echo "Instalando pilha de Desenvolvimento JAVA:"
+echo "Instalando browsers"
 
-	add-apt-repository ppa:webupd8team/java
-	
-	apt-get update
-	
-	#apt-get install oracle-java9-installer
-	#java9 já está desatualizado 
+apt-get install chromium
 
-	wget http://download.netbeans.org/netbeans/8.2/final/zip/netbeans-8.2-201609300101.zip -O netbeans.zip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-	unzip netbeans.zip -d /opt/
+apt install ./google-chrome-stable_current_amd64.deb
 
-	mv /opt/netbeans*/ /opt/netbeans/
-
-	ln -sf /opt/netbeans/bin/netbeans /usr/bin/netbeans
-
-	echo -e '[Desktop Entry]\n Version=1.0\n Name=netbeans\n Exec=/opt/netbeans/bin/netbeans\n Icon=/opt/netbeans/nb/netbeans.png\n Type=Application\n Categories=Application' | sudo tee /usr/share/applications/netbeans.desktop
-
-echo "Instalando pilha do Android Studio:"
-
-	add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y && apt-get update && apt-get install ubuntu-make -y
-
-	umake android
-
-	apt-get install android-tools-adb
-
-echo "Instalando Telegram:"
-
-	add-apt-repository ppa:atareao/telegram
-
-	apt-get update
-
-	apt-get install telegram
-
-echo "Spotify:"
-
-	sh -c "echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list.d/spotify.list"
-
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-
-	apt-get update
-
-	apt-get install spotify-client
+rm -rf google-chrome-stable_current_amd64.deb
 
 echo "Instalando pilha de Desenvolvimento Python:"
 
@@ -64,25 +29,28 @@ echo "Instalando pilha de Desenvolvimento Python:"
 
 	pip3 install jupyter
 
-	add-apt-repository ppa:mystic-mirage/pycharm
-
-	apt update
-
-	apt install pycharm-community 
-
-echo "Instalando Atom:"
-
-	add-apt-repository ppa:webupd8team/atom -y 
-
-	apt-get update -y
-
-	apt-get install atom -y
+	pip3 install virtualenv	
 
 echo "Ferramentas para o modo texto:"
 
 	apt-get install guake -y
 
 	apt-get install tmux -y
+
+
+echo "Instalando Visual Studio Code"
+
+	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+	install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+
+	sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+	apt-get install apt-transport-https
+
+	apt-get update
+
+	apt-get install code
 
 echo "Limpando:"
 
