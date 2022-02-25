2ed63917730ceec7af04f0eba80b4f9eb0c84c3d
juarezhenriquelisboa <juarezhenriquelisboa@gmail.com>
2022-02-25 11:03:13 -0500

    adicionando sublime text

--- script.sh
+++ script.sh
@@ -17,6 +17,16 @@ echo "Ferramentas para o modo texto:"
 
 	apt-get install tmux -y
 
+echo "Instalando Sublime Text:"
+
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+	apt-get update
+
+	apt-get install sublime-text
+
 echo "Limpando:"
 
 	apt-get clean
